1c37b810046136db65946c7f78027d3df832f8f6
Rodrigo Alves <rodrigo54mix@gmail.com>
2016-12-23 01:06:46 -0300

    uglify js e css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
 var gulp = require('gulp');
 var processhtml = require('gulp-processhtml');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var ngAnnotate = require('gulp-ng-annotate');
 
 gulp.task('html', function () {
     var opts = { /* plugin options */ };
@@ -17,8 +21,40 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./css'));
 });
 
-// define tasks here
-gulp.task('default', function(){
-  // run tasks here
-  // set up watch handlers here
+gulp.task('css', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./dist'));
 });
+
+
+gulp.task('LibsJS', function() {
+  var srcFiles = [
+    './lib/angular/angular.js',
+    './lib/angular-ui-router/release/angular-ui-router.js',
+    './lib/angular-aria/angular-aria.js',
+    './lib/angular-animate/angular-animate.js',
+    './lib/angular-messages/angular-messages.js',
+    './lib/angular-material/angular-material.js',
+    './lib/ngstorage/ngStorage.js',
+    './lib/angular-material-sidemenu/dest/angular-material-sidemenu.js',
+    ];
+  return gulp.src(srcFiles)
+    .pipe(concat('concat.js'))
+    .pipe(uglify())
+    .pipe(rename('libs.min.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('AppJS', function() {
+  return gulp.src('./js/**/*.js')
+    .pipe(concat('concat.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+// define tasks here
+gulp.task('default', ['LibsJS', 'AppJS', 'css','html']);
